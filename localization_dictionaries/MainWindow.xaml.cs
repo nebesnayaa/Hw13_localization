bc61993579db0d235c86cb0cea9a740565607101
nebesnayaa <144495357+nebesnayaa@users.noreply.github.com>
2024-01-02 01:32:32 +0200

    Add files via upload

---
+++ localization_dictionaries/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HW13_localization_languages
+{
+    public partial class MainWindow : Window
+    {
+        string Language { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Language = "en";
+        }
+        private void OKButton_Click(object sender, RoutedEventArgs e)
+        {
+            firstNameTextBox.Text = "";
+            lastNameTextBox.Text = "";
+            phoneNumberTextBox.Text = "";
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            firstNameTextBox.Text = "";
+            lastNameTextBox.Text = "";
+            phoneNumberTextBox.Text = "";
+        }
+
+        private void ChangeLanguageButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Language == "en")
+            {
+                ResourceDictionary newDictionary = new ResourceDictionary();
+                newDictionary.Source = new Uri("Resources/Dictionary_ua.xaml", UriKind.Relative);
+                Application.Current.Resources.MergedDictionaries[0] = newDictionary;
+                Language = "ua";
+            }
+            else if (Language == "ua")
+            {
+                ResourceDictionary newDictionary = new ResourceDictionary();
+                newDictionary.Source = new Uri("Resources/Dictionary_en.xaml", UriKind.Relative);
+                Application.Current.Resources.MergedDictionaries[0] = newDictionary;
+                Language = "en";
+            }
+        }
+    }
+}
---
+++ localization_resource_files/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System.Globalization;
+using System.Threading;
+using System.Windows;
+
+namespace HW13_localization2
+{
+    public partial class MainWindow : Window
+    {
+        string Language { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            Language = "en";
+        }
+
+        private void UpdateUI()
+        {
+            mainWindow.Title = lang.windowTitle;
+            firstNameLabel.Content = lang.firstNameLabel;
+            lastNameLabel.Content = lang.lastNameLabel;
+            phoneNumberLabel.Content = lang.phoneNumberLabel;
+            okButton.Content = lang.okButton;
+            cancelButton.Content = lang.cancelButton;
+            changeLanguageButton.Content = lang.changeLanguageButton;
+        }
+        private void OKButton_Click(object sender, RoutedEventArgs e)
+        {
+            firstNameTextBox.Text = "";
+            lastNameTextBox.Text = "";
+            phoneNumberTextBox.Text = "";
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            firstNameTextBox.Text = "";
+            lastNameTextBox.Text = "";
+            phoneNumberTextBox.Text = "";
+        }
+
+        private void ChangeLanguageButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Language == "en")
+            {
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo("ua");
+                UpdateUI();
+                Language = "ua";
+            }
+            else
+            {
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo("en");
+                UpdateUI();
+                Language = "en";
+            }
+        }
+    }
+}
